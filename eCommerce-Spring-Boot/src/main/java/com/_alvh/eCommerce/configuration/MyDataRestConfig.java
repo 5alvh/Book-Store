2c44279995e926ca9da872609a66445fce965778
Salah eddine khouadri <salahidslhddnn@gmail.com>
2025-03-04 19:58:10 +0100

    checkout v2

--- eCommerce-Spring-Boot/src/main/java/com/_alvh/eCommerce/configuration/MyDataRestConfig.java
+++ eCommerce-Spring-Boot/src/main/java/com/_alvh/eCommerce/configuration/MyDataRestConfig.java
@@ -1,8 +1,10 @@
 package com._alvh.eCommerce.configuration;
 
+import com._alvh.eCommerce.entity.Country;
 import com._alvh.eCommerce.entity.Product;
 import com._alvh.eCommerce.entity.ProductCategory;
 
+import com._alvh.eCommerce.entity.State;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.metamodel.EntityType;
 
@@ -13,6 +15,7 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.core.mapping.ExposureConfigurer;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -34,19 +37,26 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
 
         // disable HTTP methods for Product: PUT, POST and DELETE
-        config.getExposureConfiguration()
-                .forDomainType(Product.class)
-                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+        disbleHttpMethods(config.getExposureConfiguration()
+                .forDomainType(Product.class), theUnsupportedActions);
 
-        // disable HTTP methods for ProductCategory: PUT, POST and DELETE
-        config.getExposureConfiguration()
-                .forDomainType(ProductCategory.class)
-                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+        disbleHttpMethods(config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class), theUnsupportedActions);
+
+        disbleHttpMethods(config.getExposureConfiguration()
+                .forDomainType(State.class), theUnsupportedActions);
+
+        disbleHttpMethods(config.getExposureConfiguration()
+                .forDomainType(Country.class), theUnsupportedActions);
 
         // call an internal helper method
         exposeIds(config);
+    }
+
+    private static void disbleHttpMethods(ExposureConfigurer config, HttpMethod[] theUnsupportedActions) {
+        config
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
     }
 
     private void exposeIds(RepositoryRestConfiguration config) {
--- ecommerce-Angular/src/app/components/checkout/checkout.component.ts
+++ ecommerce-Angular/src/app/components/checkout/checkout.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MyFormService } from '../../services/my-form.service';
+import { Country } from '../../common/country';
+import { State } from '../../common/state';
 
 @Component({
   selector: 'app-checkout',
@@ -14,8 +16,14 @@ export class CheckoutComponent implements OnInit {
   checkoutFormGroup!: FormGroup;
   totalPrice: number = 0;
   totalQuantity: number = 0;
+
   creditCardMonths: number[] = [];
   creditCardYears: number[] = [];
+
+  countries: Country[]=[];
+  shippingAdressStates: State[] = [];
+  billingAdressStates: State[] = [];
+
   constructor(private formBuilder: FormBuilder, private myFormService: MyFormService) { }
   ngOnInit(): void {
     this.checkoutFormGroup = this.formBuilder.group({
@@ -58,20 +66,34 @@ export class CheckoutComponent implements OnInit {
     this.myFormService.getCreditCardYears().subscribe(
       data => this.creditCardYears = data
     )
+
+    //populate countries
+    this.myFormService.getCountries().subscribe(
+      
+      data =>{
+        console.log("Retrieved countries: "+JSON.stringify(data)),
+        this.countries = data
+      } 
+    )
   }
 
   onSubmit() {
     console.log("Handling the submit button");
     console.log(this.checkoutFormGroup.get('customer')?.value);
     console.log("The Email Adress is "+this.checkoutFormGroup.get('customer')?.value.email);
+    
+    console.log("The shipping address country is "+this.checkoutFormGroup.get('shippingAdress')?.value.country.name);
+    console.log("The shipping address state is "+this.checkoutFormGroup.get('shippingAdress')?.value.state.name);
   }
   
   copyShippingAdressToBillingAdress(event: Event) {
 
     if ((event.target as HTMLInputElement)?.checked) {
       this.checkoutFormGroup.controls['billingAdress'].setValue(this.checkoutFormGroup.controls['shippingAdress'].value);
+      this.billingAdressStates = this.shippingAdressStates;
     } else {
       this.checkoutFormGroup.controls['billingAdress'].reset();
+      this.billingAdressStates = [];
     }
   }
 
@@ -87,6 +109,27 @@ export class CheckoutComponent implements OnInit {
 
     this.myFormService.getCreditCardMonths(startMonth).subscribe(
       data => this.creditCardMonths = data
+    )
+  }
+  handleStates(formGroupName: string) {
+    const formGroup = this.checkoutFormGroup.get(formGroupName);
+
+    const countryCode = formGroup?.value.country.code;
+    const countryName = formGroup?.value.country.name;
+    
+    console.log(`${formGroupName} country code: ${countryCode}, name: ${countryName}` )
+    
+    this.myFormService.getStates(countryCode).subscribe(
+      data => {
+        if (formGroupName === 'shippingAdress') {
+          this.shippingAdressStates = data;
+        } else {
+          this.billingAdressStates = data;
+        }
+
+        //select the first state as default
+        formGroup?.get('state')?.setValue(data[0])
+      }
     )
   }
 }
